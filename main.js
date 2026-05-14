2931031997b9c94d18a1c641d1a44efbf57681c2
Dublonx <dublonjmx17@gmail.com>
2026-05-14 20:42:29 +0800

    Added functionality for menu bar

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 // navbar
+const menu = document.querySelector('#menu');
+const nav = document.querySelector('#nav');
+const links = document.querySelectorAll('.link');
+
+menu.addEventListener("click", () => {
+ nav.classList.toggle("active");
+ 
+ // x-icon
+ if (nav.classList.contains("active")) {
+  menu.className = "fa-solid fa-xmark";
+ } else {
+  menu.className = "fa-solid fa-bars";
+ }
+});
+
+// close navbar when the user clicks any links
+links.forEach(link => {
+ link.addEventListener("click", () => {
+  nav.classList.remove("active");
+  
+  // icon
+  menu.className = "fa-solid fa-bars";
+ });
+});
